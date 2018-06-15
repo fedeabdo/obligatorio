3620b0cc6d33818494543fb73cc78e7d2aa02dea
VKunze <39528476+VKunze@users.noreply.github.com>
2018-06-15 11:08:35 -0300

    "Agregue el readme"

--- grupo1-p2/test/testReporte.java
+++ grupo1-p2/test/testReporte.java
@@ -21,53 +21,53 @@ public class testReporte {
 		System.out.println("Reportes : " + (finishTime-startTime) + "  milisegundos");
 	}
 	
-	@Test
-	public void testReporte1() throws InvalidFile, EmptyQueueException {
-		long startTime = System.currentTimeMillis();
-		Reporte reporte = new Reporte("tabla_datos.CSV");
-		long finishTime = System.currentTimeMillis();
-		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
-		startTime = System.currentTimeMillis();
-		reporte.reporte1();
-		finishTime = System.currentTimeMillis();
-		System.out.println("Reporte1 : " + (finishTime-startTime) + "  milisegundos");
-	}
-	
-	@Test
-	public void testReporte2() throws InvalidFile, EmptyQueueException {
-		long startTime = System.currentTimeMillis();
-		Reporte reporte = new Reporte("tabla_datos.CSV");
-		long finishTime = System.currentTimeMillis();
-		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
-		startTime = System.currentTimeMillis();
-		reporte.reporte2();
-		finishTime = System.currentTimeMillis();
-		System.out.println("Reporte2 : " + (finishTime-startTime) + "  milisegundos");
-		
-	}
-
-	@Test
-	public void testResporte3() throws InvalidFile {
-		long startTime = System.currentTimeMillis();
-		Reporte reporte = new Reporte("tabla_datos.CSV");
-		long finishTime = System.currentTimeMillis();
-		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
-		startTime = System.currentTimeMillis();
-		reporte.reporte3();
-		finishTime = System.currentTimeMillis();
-		System.out.println("Reporte3 : " + (finishTime-startTime) + "  milisegundos");
-	}
-	
-	@Test
-	public void testReporte4() throws InvalidFile {
-		long startTime = System.currentTimeMillis();
-		Reporte reporte = new Reporte("tabla_datos.CSV");
-		long finishTime = System.currentTimeMillis();
-		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
-		startTime = System.currentTimeMillis();
-		reporte.reporte4();
-		finishTime = System.currentTimeMillis();
-		System.out.println("Reporte4 : " + (finishTime-startTime) + "  milisegundos");
-	}
+//	@Test
+//	public void testReporte1() throws InvalidFile, EmptyQueueException {
+//		long startTime = System.currentTimeMillis();
+//		Reporte reporte = new Reporte("tabla_datos.CSV");
+//		long finishTime = System.currentTimeMillis();
+//		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
+//		startTime = System.currentTimeMillis();
+//		reporte.reporte1();
+//		finishTime = System.currentTimeMillis();
+//		System.out.println("Reporte1 : " + (finishTime-startTime) + "  milisegundos");
+//	}
+//	
+//	@Test
+//	public void testReporte2() throws InvalidFile, EmptyQueueException {
+//		long startTime = System.currentTimeMillis();
+//		Reporte reporte = new Reporte("tabla_datos.CSV");
+//		long finishTime = System.currentTimeMillis();
+//		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
+//		startTime = System.currentTimeMillis();
+//		reporte.reporte2();
+//		finishTime = System.currentTimeMillis();
+//		System.out.println("Reporte2 : " + (finishTime-startTime) + "  milisegundos");
+//		
+//	}
+//
+//	@Test
+//	public void testResporte3() throws InvalidFile {
+//		long startTime = System.currentTimeMillis();
+//		Reporte reporte = new Reporte("tabla_datos.CSV");
+//		long finishTime = System.currentTimeMillis();
+//		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
+//		startTime = System.currentTimeMillis();
+//		reporte.reporte3();
+//		finishTime = System.currentTimeMillis();
+//		System.out.println("Reporte3 : " + (finishTime-startTime) + "  milisegundos");
+//	}
+//	
+//	@Test
+//	public void testReporte4() throws InvalidFile {
+//		long startTime = System.currentTimeMillis();
+//		Reporte reporte = new Reporte("tabla_datos.CSV");
+//		long finishTime = System.currentTimeMillis();
+//		System.out.println("Carga : " + (finishTime-startTime) + "  milisegundos");
+//		startTime = System.currentTimeMillis();
+//		reporte.reporte4();
+//		finishTime = System.currentTimeMillis();
+//		System.out.println("Reporte4 : " + (finishTime-startTime) + "  milisegundos");
+//	}
 
 }
